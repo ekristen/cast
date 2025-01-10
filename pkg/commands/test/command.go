cdd5901ca2424b5c0a186399aa2936f1a562da69
Erik Kristensen <erik@erikkristensen.com>
2025-01-10 09:37:57 -0700

    feat: switch to native go for docker and test-state

--- pkg/commands/test/command.go
+++ pkg/commands/test/command.go
@@ -1,17 +1,26 @@
 package test
 
 import (
-	"errors"
+	"context"
 	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"runtime"
+	"syscall"
+
+	"github.com/urfave/cli/v2"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+
 	"github.com/ekristen/cast/pkg/commands"
 	"github.com/ekristen/cast/pkg/common"
 	"github.com/ekristen/cast/pkg/config"
-	"github.com/urfave/cli/v2"
-	"math/rand"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"runtime"
 )
 
 func Execute(c *cli.Context) error {
@@ -54,27 +63,98 @@ func Execute(c *cli.Context) error {
 
 	name := fmt.Sprintf("cast-test-state-%s", randomString(9))
 
-	args := []string{
-		"run", "-i", "--rm",
-		fmt.Sprintf(`--name=%s`, name),
-		fmt.Sprintf("--volume=%s:/srv/salt/%s", basePath, cfg.Manifest.Name),
-		`--cap-add=SYS_ADMIN`,
-		fmt.Sprintf("--platform=%s", c.String("platform")),
-		c.String("image"),
-		"salt-call", "-l", "debug", "--local", "--retcode-passthrough",
-		"--state-output=mixed", "state.sls", state,
+	/*
+		args := []string{
+			"run", "-i", "--rm",
+			fmt.Sprintf(`--name=%s`, name),
+			fmt.Sprintf("--volume=%s:/srv/salt/%s", basePath, cfg.Manifest.Name),
+			`--cap-add=SYS_ADMIN`,
+			fmt.Sprintf("--platform=%s", c.String("platform")),
+			c.String("image"),
+			"salt-call", "-l", "debug", "--local", "--retcode-passthrough",
+			"--state-output=mixed", "state.sls", state,
+		}
+
+		cmd := exec.CommandContext(c.Context, "docker", args...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			var exitError *exec.ExitError
+			if errors.As(err, &exitError) {
+				os.Exit(exitError.ExitCode())
+			}
+			return err
+		}
+	*/
+
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return err
 	}
 
-	cmd := exec.CommandContext(c.Context, "docker", args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	ctx, cancel := context.WithCancel(c.Context)
+	defer cancel()
 
-	if err := cmd.Run(); err != nil {
-		var exitError *exec.ExitError
-		if errors.As(err, &exitError) {
-			os.Exit(exitError.ExitCode())
-		}
+	// Pull the image
+	imageOut, err := dockerClient.ImagePull(ctx, c.String("image"), image.PullOptions{})
+	if err != nil {
 		return err
+	}
+	defer imageOut.Close()
+
+	// Display image pull progress
+	_, err = io.Copy(os.Stdout, imageOut)
+	if err != nil {
+		return err
+	}
+
+	resp, err := dockerClient.ContainerCreate(ctx, &container.Config{
+		Image: c.String("image"),
+		Cmd: []string{
+			"salt-call", "-l", "debug", "--local", "--retcode-passthrough",
+			"--state-output=mixed", "state.sls", state,
+		},
+	}, &container.HostConfig{
+		AutoRemove: true,
+		CapAdd:     []string{"SYS_ADMIN"},
+		Binds:      []string{fmt.Sprintf("%s:/srv/salt/%s", basePath, cfg.Manifest.Name)},
+	}, nil, nil, name)
+	if err != nil {
+		return err
+	}
+
+	// Handle Control-C (SIGINT) signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		_ = dockerClient.ContainerStop(ctx, resp.ID, container.StopOptions{})
+		os.Exit(1)
+	}()
+
+	if err := dockerClient.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		return err
+	}
+
+	// Stream logs in real-time
+	logOut, err := dockerClient.ContainerLogs(ctx, resp.ID, container.LogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
+	if err != nil {
+		return err
+	}
+	defer logOut.Close()
+
+	go func() {
+		_, _ = stdcopy.StdCopy(os.Stdout, os.Stderr, logOut)
+	}()
+
+	statusCh, errCh := dockerClient.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			return err
+		}
+	case <-statusCh:
 	}
 
 	return nil
