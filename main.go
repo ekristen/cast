b92aedb8b24ade5ff4b5a5ff784d4e2a4a62f31d
Erik Kristensen <erik@erikkristensen.com>
2025-12-14 12:53:50 -0700

    feat: checkpoint

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path"
 
+	"github.com/ekristen/go-checkpoint"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 
@@ -26,6 +28,20 @@ func main() {
 		}
 	}()
 
+	checkpointResult := make(chan *checkpoint.CheckResponse, 1)
+	go func() {
+		resp, err := checkpoint.Check(&checkpoint.CheckParams{
+			Product: common.NAME,
+			Version: common.SUMMARY,
+		})
+		if err != nil {
+			logrus.WithError(err).Debug("checkpoint check failed")
+			checkpointResult <- nil
+			return
+		}
+		checkpointResult <- resp
+	}()
+
 	app := cli.NewApp()
 	app.Name = path.Base(os.Args[0])
 	app.Usage = common.AppVersion.Name
@@ -44,5 +60,14 @@ func main() {
 
 	if err := app.Run(os.Args); err != nil {
 		logrus.Fatal(err)
+	}
+
+	select {
+	case resp := <-checkpointResult:
+		if resp != nil && resp.Outdated {
+			fmt.Fprintf(os.Stderr, "\nA new version of %s is available: %s (current: %s)\n", common.NAME, resp.CurrentVersion, common.SUMMARY)
+			fmt.Fprintf(os.Stderr, "Download at: %s\n", resp.CurrentDownloadURL)
+		}
+	default:
 	}
 }
